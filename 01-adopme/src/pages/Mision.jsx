11f4b051c14103f1260e2c21c9d7fa83110337dc
Ramon <34ramon.seijas@gmail.com>
2025-06-21 21:43:09 -0300

    modifciando ubicacion del Logo

--- 01-adopme/src/pages/Mision.jsx
+++ 01-adopme/src/pages/Mision.jsx
@@ -1,19 +1,18 @@
 import Header from "../components/Header";
+import "../components/Mision.css"
 
 const Mision = () => {
   return (
     <>
-    <Header>
-
-    </Header>
-    <h1>estoy aqui en Mision</h1>
+      <Header></Header>
+      <img  src="/public/adoptme.png" className="logo" alt=""   />
+      <section className="info">
+        <div className="CANTIDAD">CANTIDAD DE ADOPCIONES</div>
+        <div className="HISTORIA">NUESTRA HISTORIA</div>
+        <div className="CONDICIONES">CONDICIONES DE ENTREGA DE LOS ANIMALES</div>
+      </section>
     </>
-  )
-}
+  );
+};
 
-export default Mision
-
-
-
-
-
+export default Mision;
